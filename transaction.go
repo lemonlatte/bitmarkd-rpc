19865bb1545d345f087be5686b922478a60c9006
Jim Yeh <jim@bitmark.com>
2017-05-12 14:41:55 +0800

    Add the transfer function

--- transaction.go
+++ transaction.go
@@ -6,6 +6,8 @@ package bitmarkdClient
 
 import (
 	"encoding/json"
+
+	"github.com/bitmark-inc/bitmarkd/transactionrecord"
 )
 
 // TransactionArguments represents a transaction
@@ -18,5 +20,12 @@ func (bc *BitmarkdRPCClient) GetTransactionStatus(txId string) (json.RawMessage,
 	args := TransactionArguments{Id: txId}
 	var reply json.RawMessage
 	err := bc.call("Transaction.Status", &args, &reply)
+	return reply, err
+}
+
+// Transfer performs a bitmark transfer request
+func (bc *BitmarkdRPCClient) Transfer(transfer transactionrecord.BitmarkTransfer) (json.RawMessage, error) {
+	var reply json.RawMessage
+	err := bc.call("Bitmark.Transfer", &transfer, &reply)
 	return reply, err
 }
